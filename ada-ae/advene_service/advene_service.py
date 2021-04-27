257d41e2fcca10cf195d46ff8a93afa3b84d3944
hagt <hagt@gmx.de>
2021-04-27 14:04:11 +0200

    Fixed jsonld context. Minor improvements

--- ada-ae/advene_service/advene_service.py
+++ ada-ae/advene_service/advene_service.py
@@ -47,7 +47,7 @@ def advene_call(azp_filename, output_filename):
 
     logger.debug('stderr: {0}\n'.format(p.stderr))
     logger.debug('stdout: {0}'.format(p.stdout))
-                       
+    
     return p.returncode
     
 def update_jsonld(data):
@@ -81,19 +81,24 @@ def update_jsonld(data):
 @app.route('/convertazp', methods=['POST'])
 def convert_azp():
     temp_dir = tempfile.TemporaryDirectory()
+    logger.debug('{0}'.format(request))
     files = request.files.getlist("file")
     logger.debug('POST /convertazp {0}'.format(files))
+    if len(files) == 0:
+        logger.error('Request does not contain a "file".')
+        return jsonify(error={'message': 'Request does not contain a file.'}), 400
+    
     if len(files) > 1:
         logger.error('More than one file received')
-        return jsonify(error={'message': 'Only one file upload is supported at a time'})
+        return jsonify(error={'message': 'Only one file upload is supported at a time'}), 400
 
     file = files[0]
     filename = secure_filename(file.filename)
     
     # More sophisticatd checks could be implemented but file is already checked by the ingester
     if not filename.endswith('.azp'):
-        logger.error('Uploaded file extension is not .azp {0}'.format(filename))
-        return jsonify(error={'message': 'Uploaded file extension is not .azp '+filename})
+        logger.error('Uploaded file extension is not .azp ({0})'.format(filename))
+        return jsonify(error={'message': 'Uploaded file extension is not .azp '+filename}), 400
     
     azp_path = os.path.join(temp_dir.name, filename)
     try:
@@ -101,25 +106,26 @@ def convert_azp():
         file.save(azp_path)
     except Exception as e:
         logger.error('AZP File {0} could not be saved'.format(azp_path))
-        return jsonify(error={'message': 'AZP File '+str(azp_path)+'could not be saved', 'reason': str(e)})
+        return jsonify(error={'message': 'AZP File '+str(azp_path)+'could not be saved', 'reason': str(e)}), 500
     
     jsonld_filename = str(azp_path)+'.jsonld'
     ret = advene_call(str(azp_path), jsonld_filename)
+    logger.debug('Advene exit code: {0}'.format(ret))
     if ret != 0:
         logger.error('Advene conversion failed on {0}'.format(azp_path))
-        return jsonify(error={'message': 'Advene conversion failed on '+str(azp_path)})
+        return jsonify(error={'message': 'Advene conversion failed on '+str(azp_path)}), 500
     
     try:
         with open(jsonld_filename, 'r') as f:
             data = json.load(f) 
     except Exception as e:
         logger.error('Converted json-ld file {0} could not be loaded'.format(jsonld_filename))
-        return jsonify(error={'message': 'Converted json-ld file '+jsonld_filename+' could not be loaded', 'reason': str(e)})
+        return jsonify(error={'message': 'Converted json-ld file '+jsonld_filename+' could not be loaded', 'reason': str(e)}), 500
     
     updated_data = update_jsonld(data)
     if not updated_data:
         logger.error('Converted json-ld file {0} is corrupt and cannot be updated'.format(jsonld_filename))
-        return jsonify(error={'message': 'Converted json-ld file '+str(jsonld_filename)+' is corrupt and cannot be updated'})
+        return jsonify(error={'message': 'Converted json-ld file '+str(jsonld_filename)+' is corrupt and cannot be updated'}), 500
 
     logger.debug('Send updated json-ld content')
     return json.dumps(updated_data), 200
@@ -128,269 +134,140 @@ def convert_azp():
 def GET_JSONLD_CONTEXT():
     return '''
 {
-    "oa" : "http://www.w3.org/ns/oa#",
-    "dc" : "http://purl.org/dc/elements/1.1/",
-    "dcterms" : "http://purl.org/dc/terms/",
-    "dctypes" : "http://purl.org/dc/dcmitype/",
-    "foaf" : "http://xmlns.com/foaf/0.1/",
-    "rdf" : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "rdfs" : "http://www.w3.org/2000/01/rdf-schema#",
-    "skos" : "http://www.w3.org/2004/02/skos/core#",
-    "xsd" : "http://www.w3.org/2001/XMLSchema#",
-    "iana" : "http://www.iana.org/assignments/relation/",
-    "owl" : "http://www.w3.org/2002/07/owl#",
-    "as" : "http://www.w3.org/ns/activitystreams#",
-    "schema" : "http://schema.org/",
-    "id" : {
-      "@id" : "@id",
-      "@type" : "@id"
-    },
-    "type" : {
-      "@id" : "@type",
-      "@type" : "@id"
-    },
-    "Annotation" : "oa:Annotation",
-    "Dataset" : "dctypes:Dataset",
-    "Image" : "dctypes:StillImage",
-    "Video" : "dctypes:MovingImage",
-    "Audio" : "dctypes:Sound",
-    "Text" : "dctypes:Text",
-    "TextualBody" : "oa:TextualBody",
-    "ResourceSelection" : "oa:ResourceSelection",
-    "SpecificResource" : "oa:SpecificResource",
-    "FragmentSelector" : "oa:FragmentSelector",
-    "CssSelector" : "oa:CssSelector",
-    "XPathSelector" : "oa:XPathSelector",
-    "TextQuoteSelector" : "oa:TextQuoteSelector",
-    "TextPositionSelector" : "oa:TextPositionSelector",
-    "DataPositionSelector" : "oa:DataPositionSelector",
-    "SvgSelector" : "oa:SvgSelector",
-    "RangeSelector" : "oa:RangeSelector",
-    "TimeState" : "oa:TimeState",
-    "HttpRequestState" : "oa:HttpRequestState",
-    "CssStylesheet" : "oa:CssStyle",
-    "Choice" : "oa:Choice",
-    "Person" : "foaf:Person",
-    "Software" : "as:Application",
-    "Organization" : "foaf:Organization",
-    "AnnotationCollection" : "as:OrderedCollection",
-    "AnnotationPage" : "as:OrderedCollectionPage",
-    "Audience" : "schema:Audience",
-    "Motivation" : "oa:Motivation",
-    "bookmarking" : "oa:bookmarking",
-    "classifying" : "oa:classifying",
-    "commenting" : "oa:commenting",
-    "describing" : "oa:describing",
-    "editing" : "oa:editing",
-    "highlighting" : "oa:highlighting",
-    "identifying" : "oa:identifying",
-    "linking" : "oa:linking",
-    "moderating" : "oa:moderating",
-    "questioning" : "oa:questioning",
-    "replying" : "oa:replying",
-    "reviewing" : "oa:reviewing",
-    "tagging" : "oa:tagging",
-    "auto" : "oa:autoDirection",
-    "ltr" : "oa:ltrDirection",
-    "rtl" : "oa:rtlDirection",
-    "body" : {
-      "@id" : "oa:hasBody",
-      "@type" : "@id"
-    },
-    "target" : {
-      "@id" : "oa:hasTarget",
-      "@type" : "@id"
-    },
-    "source" : {
-      "@id" : "oa:hasSource",
-      "@type" : "@id"
-    },
-    "selector" : {
-      "@id" : "oa:hasSelector",
-      "@type" : "@id"
-    },
-    "state" : {
-      "@id" : "oa:hasState",
-      "@type" : "@id"
-    },
-    "scope" : {
-      "@id" : "oa:hasScope",
-      "@type" : "@id"
-    },
-    "refinedBy" : {
-      "@id" : "oa:refinedBy",
-      "@type" : "@id"
-    },
-    "startSelector" : {
-      "@id" : "oa:hasStartSelector",
-      "@type" : "@id"
-    },
-    "endSelector" : {
-      "@id" : "oa:hasEndSelector",
-      "@type" : "@id"
-    },
-    "renderedVia" : {
-      "@id" : "oa:renderedVia",
-      "@type" : "@id"
-    },
-    "creator" : {
-      "@id" : "dcterms:creator",
-      "@type" : "@id"
-    },
-    "generator" : {
-      "@id" : "as:generator",
-      "@type" : "@id"
-    },
-    "rights" : {
-      "@id" : "dcterms:rights",
-      "@type" : "@id"
-    },
-    "homepage" : {
-      "@id" : "foaf:homepage",
-      "@type" : "@id"
-    },
-    "via" : {
-      "@id" : "oa:via",
-      "@type" : "@id"
-    },
-    "canonical" : {
-      "@id" : "oa:canonical",
-      "@type" : "@id"
-    },
-    "stylesheet" : {
-      "@id" : "oa:styledBy",
-      "@type" : "@id"
-    },
-    "cached" : {
-      "@id" : "oa:cachedSource",
-      "@type" : "@id"
-    },
-    "conformsTo" : {
-      "@id" : "dcterms:conformsTo",
-      "@type" : "@id"
-    },
-    "items" : {
-      "@id" : "as:items",
-      "@type" : "@id",
-      "@container" : "@list"
-    },
-    "partOf" : {
-      "@id" : "as:partOf",
-      "@type" : "@id"
-    },
-    "first" : {
-      "@id" : "as:first",
-      "@type" : "@id"
-    },
-    "last" : {
-      "@id" : "as:last",
-      "@type" : "@id"
-    },
-    "next" : {
-      "@id" : "as:next",
-      "@type" : "@id"
-    },
-    "prev" : {
-      "@id" : "as:prev",
-      "@type" : "@id"
-    },
-    "audience" : {
-      "@id" : "schema:audience",
-      "@type" : "@id"
-    },
-    "motivation" : {
-      "@id" : "oa:motivatedBy",
-      "@type" : "@vocab"
-    },
-    "purpose" : {
-      "@id" : "oa:hasPurpose",
-      "@type" : "@vocab"
-    },
-    "textDirection" : {
-      "@id" : "oa:textDirection",
-      "@type" : "@vocab"
-    },
-    "accessibility" : "schema:accessibilityFeature",
-    "bodyValue" : "oa:bodyValue",
-    "format" : "dc:format",
-    "language" : "dc:language",
-    "processingLanguage" : "oa:processingLanguage",
-    "value" : "rdf:value",
-    "exact" : "oa:exact",
-    "prefix" : "oa:prefix",
-    "suffix" : "oa:suffix",
-    "styleClass" : "oa:styleClass",
-    "name" : "foaf:name",
-    "email" : "foaf:mbox",
-    "email_sha1" : "email:_sha1sum",
-    "nickname" : "foaf:nick",
-    "label" : "rdfs:label",
-    "created" : {
-      "@id" : "dcterms:created",
-      "@type" : "xsd:dateTime"
-    },
-    "modified" : {
-      "@id" : "dcterms:modified",
-      "@type" : "xsd:dateTime"
-    },
-    "generated" : {
-      "@id" : "dcterms:issued",
-      "@type" : "xsd:dateTime"
-    },
-    "sourceDate" : {
-      "@id" : "oa:sourceDate",
-      "@type" : "xsd:dateTime"
-    },
-    "sourceDateStart" : {
-      "@id" : "sourceDate:Start",
-      "@type" : "xsd:dateTime"
-    },
-    "sourceDateEnd" : {
-      "@id" : "sourceDate:End",
-      "@type" : "xsd:dateTime"
-    },
-    "start" : {
-      "@id" : "oa:start",
-      "@type" : "xsd:nonNegativeInteger"
-    },
-    "end" : {
-      "@id" : "oa:end",
-      "@type" : "xsd:nonNegativeInteger"
-    },
-    "total" : {
-      "@id" : "as:totalItems",
-      "@type" : "xsd:nonNegativeInteger"
-    },
-    "startIndex" : {
-      "@id" : "as:startIndex",
-      "@type" : "xsd:nonNegativeInteger"
-    },
+    "oa":      "http://www.w3.org/ns/oa#",
+    "dc":      "http://purl.org/dc/elements/1.1/",
+    "dcterms": "http://purl.org/dc/terms/",
+    "dctypes": "http://purl.org/dc/dcmitype/",
+    "foaf":    "http://xmlns.com/foaf/0.1/",
+    "rdf":     "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "rdfs":    "http://www.w3.org/2000/01/rdf-schema#",
+    "skos":    "http://www.w3.org/2004/02/skos/core#",
+    "xsd":     "http://www.w3.org/2001/XMLSchema#",
+    "iana":    "http://www.iana.org/assignments/relation/",
+    "owl":     "http://www.w3.org/2002/07/owl#",
+    "as":      "http://www.w3.org/ns/activitystreams#",
+    "schema":  "http://schema.org/",
+
+    "id":      {"@type": "@id", "@id": "@id"},
+    "type":    {"@type": "@id", "@id": "@type"},
+
+    "Annotation":           "oa:Annotation",
+    "Dataset":              "dctypes:Dataset",
+    "Image":                "dctypes:StillImage",
+    "Video":                "dctypes:MovingImage",
+    "Audio":                "dctypes:Sound",
+    "Text":                 "dctypes:Text",
+    "TextualBody":          "oa:TextualBody",
+    "ResourceSelection":    "oa:ResourceSelection",
+    "SpecificResource":     "oa:SpecificResource",
+    "FragmentSelector":     "oa:FragmentSelector",
+    "CssSelector":          "oa:CssSelector",
+    "XPathSelector":        "oa:XPathSelector",
+    "TextQuoteSelector":    "oa:TextQuoteSelector",
+    "TextPositionSelector": "oa:TextPositionSelector",
+    "DataPositionSelector": "oa:DataPositionSelector",
+    "SvgSelector":          "oa:SvgSelector",
+    "RangeSelector":        "oa:RangeSelector",
+    "TimeState":            "oa:TimeState",
+    "HttpRequestState":     "oa:HttpRequestState",
+    "CssStylesheet":        "oa:CssStyle",
+    "Choice":               "oa:Choice",
+    "Person":               "foaf:Person",
+    "Software":             "as:Application",
+    "Organization":         "foaf:Organization",
+    "AnnotationCollection": "as:OrderedCollection",
+    "AnnotationPage":       "as:OrderedCollectionPage",
+    "Audience":             "schema:Audience", 
+
+    "Motivation":    "oa:Motivation",
+    "bookmarking":   "oa:bookmarking",
+    "classifying":   "oa:classifying",
+    "commenting":    "oa:commenting",
+    "describing":    "oa:describing",
+    "editing":       "oa:editing",
+    "highlighting":  "oa:highlighting",
+    "identifying":   "oa:identifying",
+    "linking":       "oa:linking",
+    "moderating":    "oa:moderating",
+    "questioning":   "oa:questioning",
+    "replying":      "oa:replying",
+    "reviewing":     "oa:reviewing",
+    "tagging":       "oa:tagging",
+
+    "auto":          "oa:autoDirection",
+    "ltr":           "oa:ltrDirection",
+    "rtl":           "oa:rtlDirection",
+
+    "body":          {"@type": "@id", "@id": "oa:hasBody"},
+    "target":        {"@type": "@id", "@id": "oa:hasTarget"},
+    "source":        {"@type": "@id", "@id": "oa:hasSource"},
+    "selector":      {"@type": "@id", "@id": "oa:hasSelector"},
+    "state":         {"@type": "@id", "@id": "oa:hasState"},
+    "scope":         {"@type": "@id", "@id": "oa:hasScope"},
+    "refinedBy":     {"@type": "@id", "@id": "oa:refinedBy"},
+    "startSelector": {"@type": "@id", "@id": "oa:hasStartSelector"},
+    "endSelector":   {"@type": "@id", "@id": "oa:hasEndSelector"},
+    "renderedVia":   {"@type": "@id", "@id": "oa:renderedVia"},
+    "creator":       {"@type": "@id", "@id": "dcterms:creator"},
+    "generator":     {"@type": "@id", "@id": "as:generator"},
+    "rights":        {"@type": "@id", "@id": "dcterms:rights"},
+    "homepage":      {"@type": "@id", "@id": "foaf:homepage"},
+    "via":           {"@type": "@id", "@id": "oa:via"},
+    "canonical":     {"@type": "@id", "@id": "oa:canonical"},
+    "stylesheet":    {"@type": "@id", "@id": "oa:styledBy"},
+    "cached":        {"@type": "@id", "@id": "oa:cachedSource"},
+    "conformsTo":    {"@type": "@id", "@id": "dcterms:conformsTo"},
+    "items":         {"@type": "@id", "@id": "as:items", "@container": "@list"},
+    "partOf":        {"@type": "@id", "@id": "as:partOf"},
+    "first":         {"@type": "@id", "@id": "as:first"},
+    "last":          {"@type": "@id", "@id": "as:last"},
+    "next":          {"@type": "@id", "@id": "as:next"},
+    "prev":          {"@type": "@id", "@id": "as:prev"},
+    "audience":      {"@type": "@id", "@id": "schema:audience"},
+    "motivation":    {"@type": "@vocab", "@id": "oa:motivatedBy"},
+    "purpose":       {"@type": "@vocab", "@id": "oa:hasPurpose"},
+    "textDirection": {"@type": "@vocab", "@id": "oa:textDirection"},
+
+    "accessibility": "schema:accessibilityFeature",
+    "bodyValue":     "oa:bodyValue",
+    "format":        "dc:format",
+    "language":      "dc:language",
+    "processingLanguage": "oa:processingLanguage",
+    "value":         "rdf:value",
+    "exact":         "oa:exact",
+    "prefix":        "oa:prefix",
+    "suffix":        "oa:suffix",
+    "styleClass":    "oa:styleClass",
+    "name":          "foaf:name",
+    "email":         "foaf:mbox",
+    "email_sha1":    "foaf:mbox_sha1sum",
+    "nickname":      "foaf:nick",
+    "label":         "rdfs:label",
+
+    "created":       {"@id": "dcterms:created", "@type": "xsd:dateTime"},
+    "modified":      {"@id": "dcterms:modified", "@type": "xsd:dateTime"},
+    "generated":     {"@id": "dcterms:issued", "@type": "xsd:dateTime"},
+    "sourceDate":    {"@id": "oa:sourceDate", "@type": "xsd:dateTime"},
+    "sourceDateStart": {"@id": "oa:sourceDateStart", "@type": "xsd:dateTime"},
+    "sourceDateEnd": {"@id": "oa:sourceDateEnd", "@type": "xsd:dateTime"},
+
+    "start":         {"@id": "oa:start", "@type": "xsd:nonNegativeInteger"},
+    "end":           {"@id": "oa:end", "@type": "xsd:nonNegativeInteger"},
+    "total":         {"@id": "as:totalItems", "@type": "xsd:nonNegativeInteger"},
+    "startIndex":    {"@id": "as:startIndex", "@type": "xsd:nonNegativeInteger"},
+
     "advene" : "http://www.advene.org/ns/webannotation/",
     "local" : "http://www.advene.org/ns/_local/",
     "ao" : "'''+ONTOLOGY_PREFIX+'''",
-    "annotationType" : {
-      "@id" : "ao:annotationType",
-      "@type" : "@id"
+    "ao:annotationType": {
+        "@type": "@id"
     },
-    "annotationNumericValueSequence" : {
-      "@id" : "ao:annotationNumericValueSequence",
-      "@type" : "xsd:decimal",
-      "@container" : "@list"
+    "ao:annotationValue": {
+        "@type": "@id"
     },
-    "annotationValueSequence" : {
-      "@id" : "ao:annotationValueSequence",
-      "@type" : "@id",
-      "@container" : "@list"
-    },
-    "annotationValue" : {
-      "@id" : "ao:annotationValue",
-      "@type" : "@id"
-    },
-    "annotationNumericValue" : {
-      "@id" : "ao:annotationNumericValue",
-      "@type" : "xsd:decimal"
-    },
-    "sceneId" : "ao:sceneId"
+    "ao:annotationValueSequence": {
+        "@type": "@id"
+    }
   }
 '''
 
